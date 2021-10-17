7bb1f195e45c87458c569c1917859114e3564315
Kiran Patil <kdpatil1515@gmail.com>
2021-10-17 18:03:52 +0530

    The method is used to handle negative number exception

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -13,17 +13,26 @@ String [] arr = input.split(",|\n");
     	else  {
     		return getSum(arr);
     	}
+
     }
     
     private int getSum(String[] numbers)  {
     	int sum = 0;
     	for(int index = 0; index < numbers.length;index++) {
-    		sum = Integer.parseInt(numbers[index]) + sum;
-
-	
+    		sum = stringToInt(numbers[index]) + sum;
+    		
     	}
     	
     	return sum;
     }
-
+    private int stringToInt(String number) {
+        int num = Integer.parseInt(number);
+        if (num < 0) {
+            throw new IllegalArgumentException("Negative no not allowed");
+        } else {
+            return num;
+        }
+    }
+    
+  
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -1,10 +1,16 @@
 package calculator;
 
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.Rule;
+
 class StringCalculatorShould {
+	
+	@Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Test
     void empty_string_should_return_0() {
@@ -34,6 +40,13 @@ class StringCalculatorShould {
     void string_with_two_number_with_newLine_should_return_number_as_sum() throws Exception {
     	StringCalculator stringCalculator = new StringCalculator();
     	assertEquals(2, stringCalculator.add("1\n1"));
+    }
+    
+    @Test
+    void string_containig_negative_number() throws Exception {
+    	StringCalculator stringCalculator = new StringCalculator();
+    	thrown.expect(IllegalArgumentException.class);
+    	assertEquals(2, stringCalculator.add("-1,1"));
     }
 
 
