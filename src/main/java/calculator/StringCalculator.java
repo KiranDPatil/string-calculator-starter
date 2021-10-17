2075adbb8f6fe0950080b3326b54c7dbc384a05a
Kiran Patil <kdpatil1515@gmail.com>
2021-10-17 17:42:35 +0530

    The method is useful for Addition of two Numbers

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,11 +3,27 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-    	 if(input.isEmpty()) {
-         	return 0;
-         }
-     	return Integer.parseInt(input);
-     
+String [] arr = input.split(",");
+    	
+        if(input.isEmpty()) {
+        	return 0;
+        }else if(arr.length == 1) {
+    		return Integer.parseInt(arr[0]);
+    	}
+    	else  {
+    		return getSum(arr);
+    	}
+    }
+    
+    private int getSum(String[] numbers)  {
+    	int sum = 0;
+    	for(int index = 0; index < numbers.length;index++) {
+    		sum = Integer.parseInt(numbers[index]) + sum;
+
+	
+    	}
+    	
+    	return sum;
     }
 
 }
